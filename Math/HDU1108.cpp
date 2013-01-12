d238570e65c6c8c7be68f2133cf51e6943027472
ShadowGeeker <ShadowGeeker@gmail.com>
2013-01-12 23:37:09 +0800

    HDU1108.cpp

---
+++ Math/HDU1108.cpp
@@ -0,0 +1,38 @@
+﻿/*
+	Author: ShadowGeeker
+	GitHub: https://github.com/ShadowGeeker/
+	Blog:   http://liuke.org/
+*/
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int CalcGcd(int a, int b)
+{
+	if (a < b) swap(a, b);
+	int tmp = a % b;
+	while (tmp != 0)
+	{
+		a = b;
+		b = tmp;
+		tmp = a % b;
+	}
+	
+	return b;
+}
+
+int main(int argc, char **argv)
+{
+	int x, y;
+	while (EOF != scanf("%d %d", &x, &y))
+	{
+		int gcd = CalcGcd(x, y);
+		printf ("%d\n", x*y/gcd);
+	}
+
+	return 0;
+}
+/*
+	最小公倍数 = x * y / 最大公约数(x, y)
+	最大公约数用辗转相除法求出：gcd(x, y) = gcd(y, x%y)
+*/
