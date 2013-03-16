c58a61099dfd288bde986272dcaaf76254b554b7
ShadowGeeker <ShadowGeeker@gmail.com>
2013-03-17 00:11:04 +0800

    TrieTree

---
+++ TrieTree/HDU1075.cpp
@@ -0,0 +1,120 @@
+﻿/*
+    Author: ShadowGeeker
+    GitHub: https://github.com/ShadowGeeker/
+    Blog:   http://liuke.org/
+*/
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+
+const int nMaxSize = 26;
+class Node
+{
+public:
+    char *pTrans;
+    Node *next[nMaxSize];
+    Node()
+    {
+        pTrans = NULL;
+        for (int i = 0; i < nMaxSize; ++i)
+        {
+            next[i] = NULL;
+        }
+    }
+};
+Node root;
+
+void TreeInsert(char *pWord, char *pTrans)
+{
+    int i, len = strlen(pWord);
+    int id = 0;
+    Node *p = &root, *q = NULL;
+
+    for (i = 0; i < len; ++i)
+    {
+        id = pWord[i] - 'a';
+        if (p->next[id] == NULL)
+        {
+            q = new Node();
+            p->next[id] = q;
+        }
+        p = p->next[id];
+    }
+
+    len = strlen(pTrans) + 1;
+    p->pTrans = new char[len];
+    strcpy(p->pTrans, pTrans);
+}
+
+char* TreeQuery(char *pWord)
+{
+    int i, len = strlen(pWord);
+    int id = 0;
+    Node *p = &root;
+
+    for (i = 0; i < len; ++i)
+    {
+        id = pWord[i] - 'a';
+        if (p->next[id] == NULL)
+        {
+            return pWord;
+        }
+        p = p->next[id];
+    }
+
+    if (p->pTrans != NULL)
+    {
+        return p->pTrans;
+    }
+    return pWord;
+}
+
+void TreeDelete(Node *p)
+{
+    if (p == NULL) return ;
+
+    for (int i = 0; i < nMaxSize; ++i)
+    {
+        TreeDelete(p->next[i]);
+    }
+    if (p == &root) return ;
+    if (p->pTrans != NULL) delete p->pTrans;
+    delete p;
+}
+
+int main(int argc, char **argv)
+{
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+    char ch;
+    char szTrans[1024], szTmp[1024];
+    int i;
+
+    scanf("%s", szTmp);
+    while (scanf("%s", szTrans))
+    {
+        if (strcmp("END", szTrans) == 0) break;
+        scanf("%s", szTmp);
+        TreeInsert(szTmp, szTrans);
+    }
+    scanf("%s%*c", szTmp);
+    i = 0;
+    while (1)
+    {
+        szTmp[i] = getchar();
+        if (!(szTmp[i] >= 'a' && szTmp[i] <= 'z'))
+        {
+            if (szTmp[i] == 'E') break;
+            ch = szTmp[i];
+            szTmp[i] = 0;
+            printf("%s%c", TreeQuery(szTmp), ch);
+            i = -1;
+        }
+        ++i;
+    }
+    TreeDelete(&root);
+
+    return 0;
+}
---
+++ TrieTree/HDU1800.cpp
@@ -0,0 +1,89 @@
+﻿/*
+    Author: ShadowGeeker
+    GitHub: https://github.com/ShadowGeeker/
+    Blog:   http://liuke.org/
+*/
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+
+const int nMaxSize = 10;
+struct Node
+{
+    int count;
+    Node *next[nMaxSize];
+};
+Node root = {0};
+int g_nMaxRes = 0;
+
+void TreeInsert(char *pStr)
+{
+    Node *p = &root, *q = NULL;
+    int i, j, len = strlen(pStr);
+    int id = 0;
+
+    for (i = 0; i < len; ++i)
+    {
+        id = pStr[i] - '0';
+        if (p->next[id] == NULL)
+        {
+            q = new Node();
+            for (j = 0; j < nMaxSize; ++j)
+            {
+                q->next[j] = NULL;
+            }
+            q->count = 0;
+            p->next[id] = q;
+        }
+        p = p->next[id];
+    }
+
+    p->count++;
+    if (g_nMaxRes < p->count)
+    {
+        g_nMaxRes = p->count;
+    }
+}
+
+void TreeDelete(Node *p)
+{
+    if (p == NULL) return ;
+    for (int i = 0; i < nMaxSize; ++i)
+    {
+        TreeDelete(p->next[i]);
+    }
+
+    if (p == &root) return ;
+    delete p;
+    p = NULL;
+}
+
+int main(int argc, char **argv)
+{
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+
+    char szStr[32];
+    int i, n;
+    char *p = NULL;
+
+    while (EOF != scanf("%d%*c", &n))
+    {
+        g_nMaxRes = 0;
+        memset(&root, 0, sizeof(root));  // 奇葩点
+        for (i = 0; i < n; ++i)
+        {
+            gets(szStr);
+            p = szStr;
+            while (*p == '0') ++p;
+            TreeInsert(p);
+        }
+        printf("%d\n", g_nMaxRes);
+        TreeDelete(&root);
+    }
+
+    return 0;
+}
+// 扩展阅读：http://www.programlife.net/debugger-magic-number.html
